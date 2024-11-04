06506eafe63cbf70fa3061184fac802e10c232c5
kirra <kirilkaq44@yandex.ru>
2024-11-04 15:06:55 +0300

    unclude_php_add

---
+++ app/include/header.php
@@ -0,0 +1,36 @@
+<header class="container-fluid">
+    <div class="container">
+        <div class = "row">
+            <div class="col-4">
+                <h1>
+                    <a href="<?php echo BASE_URL ?>">Интервьюирование</a></h1>
+            </div>
+            <nav class="col-8">
+                <ul>
+                    <li><a href="<?php echo BASE_URL ?>">
+                            <img src="assets/icons/home.png" alt="Главная" style="width: 30px; height: 30px;" />
+                            Главная
+                        </a></li>
+                    <li><a href="#">
+                            <img src="assets/icons/mobile.png" alt="Контакты приемной комиссии" style="width: 30px; height: 30px;" />
+                            Контакты приемной комиссии
+                        </a></li>
+                    <li><a href="#">
+                            <img src="assets/icons/usluga.png" alt="Контакты приемной комиссии" style="width: 30px; height: 30px;" />
+                            Услуги
+                        </a></li>
+                    <li>
+                        <a href="#">
+                            <img src="assets/icons/user.png" alt="Кабинет" style="width: 30px; height: 30px;" />
+                            Кабинет
+                        </a>
+                        <ul>
+                            <li><a href="log.php">Админ панель</a></li>
+                            <li><a href="log.php">Выход</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
--- single.php
+++ single.php
@@ -1,3 +1,5 @@
+<?php include("path.php"); ?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -29,42 +31,7 @@
 </head>
 <body>
 
-<header class="container-fluid">
-  <div class="container">
-    <div class = "row">
-      <div class="col-4">
-        <h1>
-          <a href="index.php">Интервьюирование</a></h1>
-      </div>
-      <nav class="col-8">
-        <ul>
-          <li><a href="index.php">
-            <img src="assets/icons/home.png" alt="Главная" style="width: 30px; height: 30px;" />
-            Главная
-          </a></li>
-          <li><a href="#">
-            <img src="assets/icons/mobile.png" alt="Контакты приемной комиссии" style="width: 30px; height: 30px;" />
-            Контакты приемной комиссии
-          </a></li>
-          <li><a href="#">
-            <img src="assets/icons/usluga.png" alt="Контакты приемной комиссии" style="width: 30px; height: 30px;" />
-            Услуги
-          </a></li>
-          <li>
-            <a href="#">
-              <img src="assets/icons/user.png" alt="Кабинет" style="width: 30px; height: 30px;" />
-              Кабинет
-            </a>
-            <ul>
-              <li><a href="log.php">Админ панель</a></li>
-              <li><a href="log.php">Выход</a></li>
-            </ul>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </div>
-</header>
+<?php include("app/include/header.php"); ?>
 
 
 
@@ -167,46 +134,7 @@
 <!--Блок Main end-->
 
 
-<!--Footer-->
-<!-- Содержание страницы -->
-<div class="footer-content">
-  <!-- Ваше основное содержимое страницы -->
-</div>
-
-<!-- Линия-разделитель -->
-<hr>
-
-<footer>
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-4">
-        <h3>О нас</h3>
-        <p>Брянский государственный университет имени академика И.Г. Петровского.
-          БГУ - вуз для тех, кто верит в себя и стремится к успеху!</p>
-      </div>
-      <div class="col-sm-4">
-        <h3>Полезные ссылки</h3>
-        <ul>
-          <li><a href="#">Главная</a></li>
-          <li><a href="#">О компании</a></li>
-          <li><a href="#">Контакты</a></li>
-        </ul>
-      </div>
-      <div class="col-sm-4">
-        <h3>Свяжитесь с нами</h3>
-        <p>Адрес: 241036, г. Брянск, ул. Бежицкая, 14, главный корпус,
-          каб. 111</p>
-        <p>Телефон: 8 (4832) 58-90-85</p>
-        <p>Email:  priembgu@mail.ru</p>
-      </div>
-    </div>
-    <div class="copyright">
-      <p>&copy; 2024 Ваша компания. Все права защищены.</p>
-    </div>
-  </div>
-</footer>
-<!--Footer end-->
-
+<?php include("app/include/footer.php"); ?>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
