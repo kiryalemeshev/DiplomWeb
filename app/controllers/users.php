bb3f9a779f69763dd6084fc571cb3ad34ac6fe6e
kirra <kirilkaq44@yandex.ru>
2024-11-28 16:32:08 +0300

    add post and get modification

--- app/controllers/users.php
+++ app/controllers/users.php
@@ -1,25 +1,46 @@
 <?php
 include "app/database/db.php";
 
-if (isset($_POST['login'])){
-    $login = $_POST['login'];
-    $email = $_POST['mail'];
-    $pass = password_hash($_POST['pass-second'] , PASSWORD_DEFAULT);
+
+$errMsg = '';
+
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+
     $admin = 0;
+    $login = trim($_POST['login']);
+    $email = trim($_POST['mail']);
+    $passF = trim($_POST['pass-first']);
+    $passS = trim($_POST['pass-second']);
 
+    if($login === '' || $email === '' || $passF === ''){
+        $errMsg = "Не все поля заполнены!";
+    }elseif (mb_strlen($login, 'UTF-8') < 2){
+        $errMsg = "Логин должен быть более двух символов!!!";
+    }elseif ($passF !== $passS)
+    {
+        $errMsg = "Пароли в обоих полях должны совпадать!!!";
+    }
 
+    else{
+        $pass = password_hash($passF, PASSWORD_DEFAULT);
+        $post =  [
+            'admin' => $admin,
+            'username' => $login,
+            'email' => $email,
+            'password' => $pass
 
-    $post =  [
-      'admin' => $admin,
-        'username' => $login,
-        'email' => $email,
-        'password' => $pass
+        ];
 
-    ];
+        //$id = insert("users",$post);
+        tt($post);
+    }
 
-    $id = insert("users",$post);
-    $last_row = selectOne("users",["id"=>$id]);
+    //$last_row = selectOne("users",["id"=>$id]);
 
-
-
+} else {
+    echo 'GET';
+    $login = '';
+    $email = '';
 }
+
+ //$pass = password_hash($_POST['pass-second'] , PASSWORD_DEFAULT);
